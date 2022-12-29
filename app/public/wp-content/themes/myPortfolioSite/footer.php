8943b24da0a7ca288170a1d9bd63c4b6deb4da6c
R-Nishitsuji <ryohmaorama@gmail.com>
2022-12-29 14:25:43 +0900

    a

---
+++ app/public/wp-content/themes/myPortfolioSite/footer.php
@@ -0,0 +1,9 @@
+<footer id="footer">
+    <div class="footer">© Ryoma Nishitsuji</div>
+    <?php wp_footer(); ?>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
